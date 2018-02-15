3d84db2649d85e00dc97309cbc361a719f0c4e1c
Leonardo <ferreira.leonardo@me.com>
2018-02-15 19:56:26 -0200

    Initial commit

---
+++ aDigitalWebSite.Web/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aDigitalWebSite.Web
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var host = BuildWebHost(args);
+			host.Run();
+		}
+
+		public static IWebHost BuildWebHost(string[] args)
+		{
+			return WebHost.CreateDefaultBuilder(args)
+					.UseStartup<Startup>()
+					.UseKestrel()
+			   		.UseUrls(new string[] { "http://0.0.0.0:5001", "http://10.0.0.225:5001" })
+					.Build();
+		}
+	}
+}
