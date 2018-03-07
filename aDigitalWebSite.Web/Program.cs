14156e79716c8d09623a609e172bcc55c0d696d4
leonardo <index.xp@gmail.com>
2018-03-06 21:49:48 -0300

    Added Application Insights

--- aDigitalWebSite.Web/Program.cs
+++ aDigitalWebSite.Web/Program.cs
@@ -20,11 +20,22 @@ namespace aDigitalWebSite.Web
 
 		public static IWebHost BuildWebHost(string[] args)
 		{
+#if DEBUG
 			return WebHost.CreateDefaultBuilder(args)
+							  .UseApplicationInsights("77fec417-0965-4c97-868b-8237ceaa8b24")
+							.UseStartup<Startup>()
+							.UseKestrel()
+					   		.UseUrls(new string[] { "http://0.0.0.0:5001", "http://10.0.0.225:5001" })
+							.Build();
+#else
+			return WebHost.CreateDefaultBuilder(args)
+					.UseApplicationInsights("77fec417-0965-4c97-868b-8237ceaa8b24")
 					.UseStartup<Startup>()
 					.UseKestrel()
-			   		.UseUrls(new string[] { "http://0.0.0.0:5001", "http://10.0.0.225:5001" })
+			   		.UseUrls(new string[] { "http://adigital.press" })
 					.Build();
+#endif
+
 		}
 	}
 }
