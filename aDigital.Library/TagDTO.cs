32da76c52acf0d666acf4ccafe5f39a121865c1d
leonardo <index.xp@gmail.com>
2018-03-27 15:16:23 -0300

    Create the new projects Started the implementation

---
+++ aDigital.Library/TagDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace aDigital.Library
+{
+	public class TagDTO
+	{
+		public TagDTO()
+		{
+		}
+		public Guid Id { get; set; }
+		public string Title { get; set; }
+		public IEnumerable<string> Synonyms { get; set; }
+		public IEnumerable<TagAssociationContext> Associations { get; set; }
+	}
+
+	public class TagAssociationContext
+	{
+		public int ContextId { get; set; }
+		public object ObjectId { get; set; }
+	}
+}
